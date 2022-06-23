f63c8bfc2e4f35a726743e182f33e7dbe0719202
Em3raud3 <animekamisama@gmail.com>
2022-06-23 09:57:24 -0400

    222

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,8 @@
 import {
   Component,
+  OnDestroy,
   OnInit,
+  ViewChild,
 
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
@@ -15,9 +17,11 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
   subscription: Subscription;
   editMode = false
   editedItemIndex: number;
+  editedItem: Ingredient;
 
 
   constructor(private slService: ShoppingListService) { }
@@ -28,6 +32,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       (index: number) => {
         this.editedItemIndex = index;
         this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
       }
     );
   }
